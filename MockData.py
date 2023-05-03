c932205bcd17623215a38050fce0c21b191c14a9
dhruvilk <38403900+dhruvilk@users.noreply.github.com>
2023-05-03 01:21:19 -0400

    testing

--- MockData.py
+++ MockData.py
@@ -145,23 +145,54 @@ for i in range(startingPatientId, finalPatientId):
 
 # ########################################################################################################################################################################
 
-# generate side effects data for x patients
-def sideEffects(x):
-    # pandas dataframe
-    sideeffects = pd.DataFrame()
-    pd.set_option('display.max_rows', None)
-    pd.set_option('display.max_columns', 500)
-    pd.set_option('max_colwidth', 400)
-    pd.set_option('display.width', 1000)
-    pd.options.display.float_format = '{:.0f}'.format
-    for i in range(0, x):
-        sideeffects.loc[i,'patientMetricsId']= int(np.random.randint(1,90))
-        sideeffects.loc[i,'patientId']= int(np.random.randint(1,90))
-        sideeffects.loc[i,'title']= str(np.random.choice(symptoms))
-    return sideeffects
-# call side effects function to generate data and place it into a variable that can be turned into a dataframe to import to excel (this can later be saved as a csv)
-effects = sideEffects(365)
-effectsdf = pd.DataFrame(effects)
-effectsdf.to_excel(r'C:\Users\dhruv\Downloads\Spring23\CS491\Fake-Data\sideeffects.xlsx', index=False) #enter a path here to save output to xlsx format
+startingPatientId = 1
+finalPatientId = 91
+outfileExtension = '.xlsx'
+outfilePath = 'C:\\Users\\dhruv\\Downloads\\Spring23\\CS491\\Fake-Data\\sideeffects'
+
+outfileExtension = '.xlsx'
+originFileName = 'sideeffects'
+originFileNumber = 1
+outfileOriginal = ("".join([originFileName, str(originFileNumber), outfileExtension]))
+
+for i in range(startingPatientId, finalPatientId):
+    outfileFinal= ("".join([outfilePath, str(startingPatientId), outfileExtension]))
+    outfileDelete = ("".join([originFileName, str(originFileNumber+1), outfileExtension]))
+    # print(outfileDelete)
+    def daterange(start_date, end_date):
+        for n in range(int((end_date - start_date).days+1)):
+            yield start_date + timedelta(n)
+    start_date = date(2023, 1, 1)
+    end_date = date(2023, 12, 31)
+
+    def testMetrics():
+        sideeffects = pd.DataFrame()
+        pd.set_option('display.max_rows', None)
+        pd.set_option('display.max_columns', 500)
+        pd.set_option('max_colwidth', 400)
+        pd.set_option('display.width', 1000)
+        pd.options.display.float_format = '{:.0f}'.format
+        for createdAtDate in daterange(start_date, end_date):
+            sideeffects.loc[createdAtDate,'patientMetricsId']= int(startingPatientId-1)
+            sideeffects.loc[createdAtDate,'patientId']= int(startingPatientId-1)
+            sideeffects.loc[createdAtDate,'title']= str(np.random.choice(symptoms))
+            sideeffects.loc[createdAtDate,'titlePercent']= int(np.random.randint(1,90))
+        return sideeffects
+
+    startingPatientId+=1
+    
+    effects = testMetrics()
+    effectsdf = pd.DataFrame(effects)
+    effectsdf.to_excel(outfileFinal, index=False) #enter a path here to save output to xlsx format
+
+
+    # read the data of excel file you want information from
+    df=pd.read_excel(outfileFinal)
+
+    # appending the data of df after the data of the excel file you want the data in
+    with pd.ExcelWriter(outfileOriginal,mode="a",engine="openpyxl",if_sheet_exists="overlay") as writer:
+        df.to_excel(writer, sheet_name="Sheet1",header=None, startrow=writer.sheets["Sheet1"].max_row,index=False)
+    originFileNumber+=1
+
 
 # ########################################################################################################################################################################
--- test.py
+++ test.py
@@ -8,22 +8,24 @@ from faker.providers import BaseProvider
 from datetime import date, timedelta
 import os
 
-moods = ['happy', 'sad', 'confused', 'frustrated', 'angry', 'tired', 'restless', 'irritable', 'impulsive']
+symptoms = ['insomnia', 'lack of appetite', 'irritability', 'impulsivity', 'weight gain', 'inactivity', 'hyperactivity']
+
+
 
 startingPatientId = 1
-finalPatientId = 10
+finalPatientId = 91
 outfileExtension = '.xlsx'
-outfilePath = 'C:\\Users\\dhruv\\Downloads\\Spring23\\CS491\\Fake-Data\\dailymetric'
+outfilePath = 'C:\\Users\\dhruv\\Downloads\\Spring23\\CS491\\Fake-Data\\sideeffects'
 
 outfileExtension = '.xlsx'
-originFileName = 'dailymetric'
+originFileName = 'sideeffects'
 originFileNumber = 1
 outfileOriginal = ("".join([originFileName, str(originFileNumber), outfileExtension]))
 
 for i in range(startingPatientId, finalPatientId):
     outfileFinal= ("".join([outfilePath, str(startingPatientId), outfileExtension]))
     outfileDelete = ("".join([originFileName, str(originFileNumber+1), outfileExtension]))
-    print(outfileDelete)
+    # print(outfileDelete)
     def daterange(start_date, end_date):
         for n in range(int((end_date - start_date).days+1)):
             yield start_date + timedelta(n)
@@ -31,30 +33,24 @@ for i in range(startingPatientId, finalPatientId):
     end_date = date(2023, 12, 31)
 
     def testMetrics():
-        testMetricsData = pd.DataFrame()
+        sideeffects = pd.DataFrame()
         pd.set_option('display.max_rows', None)
         pd.set_option('display.max_columns', 500)
         pd.set_option('max_colwidth', 400)
         pd.set_option('display.width', 1000)
         pd.options.display.float_format = '{:.0f}'.format
         for createdAtDate in daterange(start_date, end_date):
-            testMetricsData.loc[createdAtDate,'patientId']= int(startingPatientId-1)
-            testMetricsData.loc[createdAtDate,'adhd']= (np.random.randint(0,100))
-            testMetricsData.loc[createdAtDate,'anxiety']= (np.random.randint(0,100))
-            testMetricsData.loc[createdAtDate,'depression']= (np.random.randint(0,100))
-            testMetricsData.loc[createdAtDate,'createdAt']= (createdAtDate)
-            testMetricsData.loc[createdAtDate,'mood']= (np.random.choice(moods))
-            testMetricsData.loc[createdAtDate,'dosages']= int(np.random.randint(1,5))
-            testMetricsData.loc[createdAtDate,'dosagesTaken']= int(np.random.randint(1,5))
-            testMetricsData.loc[createdAtDate,'checkedIn']= bool(random.getrandbits(1))
-            testMetricsData.loc[createdAtDate,'moodLevel']= (np.random.randint(0,100))
-        return testMetricsData
+            sideeffects.loc[createdAtDate,'patientMetricsId']= int(startingPatientId-1)
+            sideeffects.loc[createdAtDate,'patientId']= int(startingPatientId-1)
+            sideeffects.loc[createdAtDate,'title']= str(np.random.choice(symptoms))
+            sideeffects.loc[createdAtDate,'titlePercent']= int(np.random.randint(1,90))
+        return sideeffects
 
     startingPatientId+=1
     
-    dailymetr = testMetrics()
-    metricsdf = pd.DataFrame(dailymetr)
-    metricsdf.to_excel(outfileFinal, index=False) #enter a path here to save output to xlsx format
+    effects = testMetrics()
+    effectsdf = pd.DataFrame(effects)
+    effectsdf.to_excel(outfileFinal, index=False) #enter a path here to save output to xlsx format
 
 
     # read the data of excel file you want information from
@@ -64,3 +60,4 @@ for i in range(startingPatientId, finalPatientId):
     with pd.ExcelWriter(outfileOriginal,mode="a",engine="openpyxl",if_sheet_exists="overlay") as writer:
         df.to_excel(writer, sheet_name="Sheet1",header=None, startrow=writer.sheets["Sheet1"].max_row,index=False)
     originFileNumber+=1
+
